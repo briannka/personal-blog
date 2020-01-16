72464b681ab70b3a0d5ae56e2137dfa7b3c9b254
Brianna Iliev <brianna.iliev@sumup.com>
2020-01-16 16:50:48 +0200

    Added items to page content

--- username.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-     event.preventDefault(); let userName=document.getElementById('username').value; 
-     alert('Username is: ' + userName); 
-     return false; 
-    }
-    );
-    
